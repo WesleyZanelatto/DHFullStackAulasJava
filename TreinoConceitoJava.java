eedaf9cd0872493c04e6dabc2b5ecae3e86490fa
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-02-14 09:56:02 -0300

    testando subindo arquivo alterado com ssh

--- TreinoConceitoJava.java
+++ TreinoConceitoJava.java
@@ -8,7 +8,9 @@ public class TreinoConceitoJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int a=0;
 		
+		System.out.println("\nValor de a = "+a);
 		
 	}
 }
