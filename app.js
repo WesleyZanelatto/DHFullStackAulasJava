8925ffd867386e6e4c2e89d8340266d54d7ca8e2
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-02-14 14:21:58 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,31 @@
+Gamer@Snp858 MINGW64 /f/Cursos/PortoSeguroDigitalHouse/1_Aulas/1_TodosProgramasDasAulasMaisOsExercícios/src/8_BaixandoArquivos
+$ git clone https://github.com/WesleyZanelatto/DHFullStackAulasJava.git
+Cloning into 'DHFullStackAulasJava'...
+remote: Enumerating objects: 24, done.
+remote: Counting objects: 100% (24/24), done.
+remote: Compressing objects: 100% (19/19), done.
+remote: Total 24 (delta 5), reused 24 (delta 5), pack-reused 0
+Receiving objects: 100% (24/24), 10.18 KiB | 3.39 MiB/s, done.
+Resolving deltas: 100% (5/5), done.
+Gamer@Snp858 MINGW64 /f/Cursos/PortoSeguroDigitalHouse/1_Aulas/1_TodosProgramasDasAulasMaisOsExercícios/src/8_BaixandoArquivos
+$ cd DHFullStackAulasJava
+Gamer@Snp858 MINGW64 /f/Cursos/PortoSeguroDigitalHouse/1_Aulas/1_TodosProgramasDasAulasMaisOsExercícios/src/8_BaixandoArquivos/DHFullStackAulasJava (master)
+$ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        app.js
+nothing added to commit but untracked files present (use "git add" to track)
+Gamer@Snp858 MINGW64 /f/Cursos/PortoSeguroDigitalHouse/1_Aulas/1_TodosProgramasDasAulasMaisOsExercícios/src/8_BaixandoArquivos/DHFullStackAulasJava (master)
+$ git add app.js
+Gamer@Snp858 MINGW64 /f/Cursos/PortoSeguroDigitalHouse/1_Aulas/1_TodosProgramasDasAulasMaisOsExercícios/src/8_BaixandoArquivos/DHFullStackAulasJava (master)
+$ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   app.js
+
+Gamer@Snp858 MINGW64 /f/Cursos/PortoSeguroDigitalHouse/1_Aulas/1_TodosProgramasDasAulasMaisOsExercícios/src/8_BaixandoArquivos/DHFullStackAulasJava (master)
+$
