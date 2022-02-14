831d633ca085ffd5dc5b9510ee248014c1e97856
Wesley Zanelatto <wesleyzanelatto@gmail.com>
2022-02-13 22:49:11 -0300

    Subindo arquivos

---
+++ TreinoConceitoJava.java
@@ -0,0 +1,22 @@
+package aulas;
+
+import java.util.Scanner;
+//import java.util.Locale;
+
+public class TreinoConceitoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+}
+	
+		
+		
+		
+		
+	
+
+
